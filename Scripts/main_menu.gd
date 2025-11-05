1bddbb3048fdeee6897157434218fedd14a74bfb
vsuho <156258471+vsuho@users.noreply.github.com>
2025-11-05 12:14:46 +0200

    Added features to options

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	main_buttons.visible = true
+	options.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,9 +21,15 @@ func _on_start_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	#print("Options pressed")
-	pass
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	#print("Exit pressed")
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,7 +12,9 @@ func _ready():
 	
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+		#get_tree().quit()
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
