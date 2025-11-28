a81325ecfd1deea1e3edf71d3c2376c267a82fe9
vsuho <156258471+vsuho@users.noreply.github.com>
2025-11-28 12:24:31 +0200

    Biting animation fixed for now

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -2,13 +2,13 @@ extends CharacterBody3D
 
 const SPEED: float = 1.5
 const GRAVITY: float = 30.0
-const STOP_DISTANCE: float = 2.0  
+const STOP_DISTANCE: float = 1.0	# Distance to player when it stops and bites
 
 @export var player: Node3D
 @onready var animation_player: AnimationPlayer = $enemy1_setup/AnimationPlayer
 
 func _physics_process(delta: float) -> void:
-	# Gravity
+# Gravity
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
 	else:
@@ -16,22 +16,28 @@ func _physics_process(delta: float) -> void:
 
 	var move_dir := Vector3.ZERO
 
+# Check if there is a player node in inspector
 	if player:
 		var to_player := player.global_transform.origin - global_transform.origin
 		to_player.y = 0
 
 		var distance := to_player.length()
+		
+		var dir_to_player = to_player.normalized()
+		var forward = -global_transform.basis.z
+		var dot = forward.dot(dir_to_player)
 
 		if distance > STOP_DISTANCE:
 			# Normalize to avoid zero vector glitch
-			move_dir = to_player / distance
-			# Play only when the animation is not playing
-			#if animation_player.current_animation != "ZombieWalk" or not animation_player.is_playing():
+			move_dir = dir_to_player
 			animation_player.play("ZombieWalk")
 			look_at(global_transform.origin + move_dir, Vector3.UP)
 		else:
-			animation_player.queue("ZombieNeckBite")
-			move_dir = Vector3.ZERO   # Keep a little distance to player
+			animation_player.play("ZombieNeckBite")
+			move_dir = Vector3.ZERO
+			look_at(global_transform.origin + dir_to_player, Vector3.UP)
+			
+			#move_dir = Vector3.ZERO   # Keep a little distance to player
 			
 
 	velocity.x = move_dir.x * SPEED
