c3d850b3106579f0003195b6e5f28ba46be250a6
Samulimv <t3vasa03@students.oamk.fi>
2025-12-02 13:52:03 +0200

    Added working key pickup and logic

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -7,12 +7,19 @@ class_name Chest
 @export var close_animation_name := "close"
 
 var is_open := false
+var on_cooldown := false  # estää useamman interaktion animaation aikana
+@export var cooldown_time := 1.0  # aika sekunteina
 
-func interact(body):
-	print(body.name, " interacted with: ", name)
+func interact(_body):
+	if on_cooldown:
+		return
+
 	if not animation_player:
 		print("AnimationPlayer missing")
 		return
+
+	on_cooldown = true
+
 	if is_open:
 		if animation_player.has_animation(close_animation_name):
 			animation_player.play(close_animation_name)
@@ -23,3 +30,13 @@ func interact(body):
 			animation_player.play(open_animation_name)
 			is_open = true
 			prompt_message = "Press F to close"
+
+	var t = Timer.new()
+	t.wait_time = cooldown_time
+	t.one_shot = true
+	t.autostart = true
+	t.connect("timeout", Callable(self, "_on_cooldown_finished"))
+	add_child(t)
+
+func _on_cooldown_finished():
+	on_cooldown = false
