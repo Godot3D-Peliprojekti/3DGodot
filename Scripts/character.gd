17abf16e157b1a708ed4919fa55302439d60c840
vsuho <156258471+vsuho@users.noreply.github.com>
2025-12-04 16:05:54 +0200

    Pistol shooting and reload sounds added

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,3 +4,11 @@ extends Node3D
 func play_footstep() -> void:
 	if owner and owner.has_method("play_footstep"):
 		owner.play_footstep()
+		
+func play_gun_reload() -> void:
+	if owner and owner.has_method("play_gun_reload"):
+		owner.play_gun_reload()
+		
+func play_gunshot() -> void:
+	if owner and owner.has_method("play_gunshot"):
+		owner.play_gunshot()
