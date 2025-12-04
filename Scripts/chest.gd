da17d723065ab5045e9a1ddf13da601ca1e97bb2
Samulimv <t3vasa03@students.oamk.fi>
2025-12-04 16:58:50 +0200

    Added 2 types of healing to randomly appear in each chest. HealthKit and PillBottle

--- Scripts/chest.gd
+++ Scripts/chest.gd
@@ -7,8 +7,30 @@ class_name Chest
 @export var close_animation_name := "close"
 
 var is_open := false
-var on_cooldown := false  # estää useamman interaktion animaation aikana
-@export var cooldown_time := 1.0  # aika sekunteina
+var on_cooldown := false
+@export var cooldown_time := 1.0
+
+@onready var pill_bottle: Node = $PillBottle
+@onready var health_kit: Node = $HealthKit
+var chosen_item: Node = null
+
+#Arvotaan pill_bottle tai health_kit laatikkoon
+func _ready():
+	randomize()
+	setup_random_item()  
+
+func setup_random_item():
+	if randi() % 2 == 0:
+		chosen_item = pill_bottle
+		pill_bottle.show()
+		health_kit.hide()
+	else:
+		chosen_item = health_kit
+		health_kit.show()
+		pill_bottle.hide()
+	
+	if chosen_item.has_method("set_pickable"):
+		chosen_item.set_pickable(false)
 
 func interact(_body):
 	if on_cooldown:
@@ -30,6 +52,10 @@ func interact(_body):
 			animation_player.play(open_animation_name)
 			is_open = true
 			prompt_message = "Press F to close"
+
+		if chosen_item:
+			if chosen_item.has_method("set_pickable"):
+				chosen_item.set_pickable(true)
 
 	var t = Timer.new()
 	t.wait_time = cooldown_time
