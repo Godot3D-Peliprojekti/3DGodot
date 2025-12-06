0a97488817ae1d9ffcf013ed926167dd929d2cef
sonkajarvi <sonkajarvi@hotmail.com>
2025-12-06 17:25:49 +0200

    separate health bar from enemy

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -1,19 +1,21 @@
 extends CharacterBody3D
 
-const SPEED: float = 2.0
 const GRAVITY: float = 30.0
+
+@export var speed: float = 0.8
 const STOP_DISTANCE: float = 1.5 	# Distance to player when it stops and bites
 const VIEW_DISTANCE: float = 5.0	# The distance that the enemy sees and reacts
 const REACTION_TIME: float = 0.4	# The reaction time of the enemy
+var time_player_in_view: float = 0.0	# The time of player in enemy's view
 
 @export var player: Node3D
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var collision_shape = $CollisionShape3D
 
-@onready var animation_tree = $enemy1_setup/AnimationTree
-@onready var animation_player: AnimationPlayer = $enemy1_setup/AnimationPlayer
+@export var health_bar: Node3D # Health_Bar_3D
 
 @export_category("Animations")
+@export var animation_tree: AnimationTree
 @export var animation_blend_easing: float = 10.0
 
 var death_blend: float
@@ -21,11 +23,6 @@ var walk_blend: float
 
 var health: int = 100
 var is_dead: bool = false
-@onready var health_bar = $Health_Bar
-@onready var progress_bar = $SubViewport/Control/ProgressBar
-@onready var health_label = $SubViewport/Control/Label
-@onready var health_indicator_label = $SubViewport2/Health_Indicator_Label
-@onready var health_indicator = $Health_Indicator
 
 func hit(damage: int) -> void:
 	health = max(health - damage, 0)
@@ -34,38 +31,17 @@ func hit(damage: int) -> void:
 		is_dead = true
 	else:
 		animation_tree["parameters/Hit_OneShot/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
-
-		health_indicator_label.text = "-" + str(damage)
-		health_indicator.position.y = 0.51
-		health_indicator.modulate.a = 1.0
+		health_bar._hit(damage)
 
 func _process(delta: float) -> void:
 	walk_blend = lerp(walk_blend, clamp(velocity.length(), 0.0, 1.0), animation_blend_easing * delta)
 	animation_tree["parameters/Walk_Blend/blend_amount"] = walk_blend
 
-	health_bar.look_at(player.global_position)
-	health_bar.global_rotation.x = 0.0
-	health_bar.global_rotation.z = 0.0
-
-	progress_bar.value = lerp(progress_bar.value, float(health), 10.0 * delta)
-	health_label.text = str(health)
-
-	health_indicator.look_at(player.global_position)
-	health_indicator.position.z = -0.1
-	health_indicator.global_rotation.x = 0.0
-	health_indicator.global_rotation.z = 0.0
-
-	health_indicator.position.y = lerp(health_indicator.position.y, 0.58, 2.0 * delta)
-	health_indicator.modulate.a = lerp(health_indicator.modulate.a, 0.0, 4.0 * delta)
-
-	var s = float(health) / 100.0
-	progress_bar.modulate.r = -s + 1
-	progress_bar.modulate.g = s
+	health_bar.health = health
+	health_bar._look_at(player.global_position)
 
 	death_blend = lerp(death_blend, float(is_dead), animation_blend_easing * delta)
 	animation_tree["parameters/Death_Blend/blend_amount"] = death_blend
-
-var time_player_in_view: float = 0.0	# The time of player in enemy's view
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
@@ -84,7 +60,6 @@ func _physics_process(delta: float) -> void:
 
 	var move_dir := Vector3.ZERO
 	var distance_to_player := (player.global_transform.origin - global_transform.origin).length()
-	
 
 	# Check if there is a player AND distance_to_player is smaller than VIEW_DISTANCE
 	if player and distance_to_player < VIEW_DISTANCE:
@@ -100,7 +75,7 @@ func _physics_process(delta: float) -> void:
 
 		var to_next := next_point - global_transform.origin
 		to_next.y = 0
-		
+
 		# Straight direction towards the player
 		var to_player := player.global_transform.origin - global_transform.origin
 		to_player.y = 0
@@ -132,8 +107,8 @@ func _physics_process(delta: float) -> void:
 				player.hit(10)
 
 		# Set the moving speed
-		velocity.x = move_dir.x * SPEED
-		velocity.z = move_dir.z * SPEED
+		velocity.x = move_dir.x * speed
+		velocity.z = move_dir.z * speed
 
 	# Player is too far away OR REACTION_TIME is up
 	elif player:
@@ -141,7 +116,6 @@ func _physics_process(delta: float) -> void:
 		to_player.y = 0
 		var dir_to_player := to_player.normalized()
 		move_dir = Vector3.ZERO		# Stay in place
-		#animation_player.play("ZombieIdle") # idle loop
 		look_at(global_transform.origin + dir_to_player, Vector3.UP)
 
 	move_and_slide()
