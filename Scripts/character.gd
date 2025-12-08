8601bb7ef846af21f2073cb7c1c7eb31b8799df3
Ville Suhonen <156258471+vsuho@users.noreply.github.com>
2025-12-08 14:48:08 +0200

    Revert "Sounds"

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,15 +4,3 @@ extends Node3D
 func play_footstep() -> void:
 	if owner and owner.has_method("play_footstep"):
 		owner.play_footstep()
-		
-func play_gun_reload() -> void:
-	if owner and owner.has_method("play_gun_reload"):
-		owner.play_gun_reload()
-		
-func play_gunshot() -> void:
-	if owner and owner.has_method("play_gunshot"):
-		owner.play_gunshot()
-
-func play_swing() -> void:
-	if owner and owner.has_method("play_swing"):
-		owner.play_swing()
