b3b19f5503123c3a4a1a39a6319b2c2c9c9b7ada
sonkajarvi <sonkajarvi@hotmail.com>
2025-12-14 05:05:07 +0200

    don't look at the ground when starting the game

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -8,32 +8,26 @@ extends Control
 func _ready():
 	main_buttons.visible = true
 	options.visible = false
-	
+
 	if not audio_stream_player.playing:
 		audio_stream_player.play()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	#audio_stream_player.stream_paused = true
 
-
 func _on_options_pressed() -> void:
 	main_buttons.visible = false
 	options.visible = true
 
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_back_options_pressed() -> void:
 	main_buttons.visible = true
 	options.visible = false
-	
